468b60552a26b0aa22c5e45ab322cd1a6641978b
aakash1122 <aakaashhossain@gmail.com>
2021-01-25 14:54:06 +0600

    add tailwind support

---
+++ tailwindcss-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "orange",
+        primary: "#05CDE9",
+        secondary: "#F2F5FA",
+        dark1: "#0D0D0D",
+        dark2: "#2E2E2E",
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  },
+};
